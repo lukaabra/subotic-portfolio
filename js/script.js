f19eafd9c235ac899b44f81a42091c161293f9bd
Luka Abramović <lukaabramovic2@gmail.com>
2020-06-25 15:08:09 +0200

    Fixed footer not sticking to bottom. Fixing some typos. Changing font sizes when resizing.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
 $(document).ready(function() {
     // Change to container-fluid if on a 4K monitor
-    if(window.innerWidth < 1800) {
+    if (window.innerWidth < 1800) {
         $("#front").addClass("container").removeClass("container-fluid");
-    } else{
+    } else {
         $("#front").addClass("container-fluid").removeClass("container");
+
+        // Turns all elements with the class 'container' to 'container-fluid' above a certain resolution
+        $("div.container").addClass("container-fluid").removeClass("container");
+        $("section.container").addClass("container-fluid").removeClass("container");
+        $("nav.container").addClass("container-fluid").removeClass("container");
+        $("#wrap").removeClass("container-fluid");
     }
 
     // Center the index-links if loading on a tablet or smaller device
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
         $(".index-link").addClass("text-center").removeClass("text-left").removeClass("text-right");
     }
 });
   
 $(window).resize(function() {
     // Change to container-fluid if resizing to a 4K monitor
-    if(window.innerWidth < 1800) {
+    if (window.innerWidth < 1800) {
         $("#front").addClass("container").removeClass("container-fluid");
-    } else{
+
+        // Turns all elements with the class 'container' to 'container-fluid' above a certain resolution
+        $("div.container").addClass("container").removeClass("container-fluid");
+        $("section.container").addClass("container").removeClass("container-fluid");
+        $("nav.container").addClass("container").removeClass("container-fluid");
+        $("#wrap").addClass("container-fluid");
+    } else {
         $("#front").addClass("container-fluid").removeClass("container");
+
+        // Turns all elements with the class 'container' to 'container-fluid' above a certain resolution
+        $("div.container").addClass("container-fluid").removeClass("container");
+        $("section.container").addClass("container-fluid").removeClass("container");
+        $("nav.container").addClass("container-fluid").removeClass("container");
+        $("#wrap").removeClass("container-fluid");
     }
 
     // Center the index-links if resizing to a tablet or smaller device
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
         $(".index-link").addClass("text-center").removeClass("text-left").removeClass("text-right");
     }
 });
